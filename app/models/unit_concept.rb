5e9d9ac94cebae7e703fce143e500a3cf12992ea
mcjung <mcjung@vovioskorea.com>
2016-02-25 20:28:40 +0900

    ddd

--- app/models/unit_concept.rb
+++ app/models/unit_concept.rb
@@ -6,7 +6,7 @@ class UnitConcept < ActiveRecord::Base
     has_many :explanations
     
     DESC_TYPES = {
-        :unit_concept_desc => "Unit Concept",
+        :unit_concept => "Unit Concept",
         :explanation => "Explanation",
         :exercise => "Exercise"
     }    
