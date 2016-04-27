aec6cb4a04132ab996a67f0a12590efef9e508ef
mcjung <mcjung@vovioskorea.com>
2016-04-27 16:35:26 +0900

    contents

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -7,6 +7,11 @@ class ContentsController < ApplicationController
         @view_type = params[:view_type].blank? ? '1' : params[:view_type]
         @step = params[:step].blank? ? '1' : params[:step]
         @category = params[:category]
+        @sub_category = params[:sub_category]
+        
+        if @step == '3'
+            @concepts = Concept.where('category = ? and sub_category = ?', @category, @sub_category)
+        end    
         
     end
 
