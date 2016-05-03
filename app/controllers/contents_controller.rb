5f185e98e096975f0ea943c1308419ec5689e758
mcjung <mcjung@vovioskorea.com>
2016-05-03 20:39:31 +0900

    content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -8,11 +8,20 @@ class ContentsController < ApplicationController
         @step = params[:step].blank? ? '1' : params[:step]
         @category = params[:category]
         @sub_category = params[:sub_category]
+        @concept_id = params[:concept_id]
         
         if @step == '3'
             @concepts = Concept.where('category = ? and sub_category = ?', @category, @sub_category)
+        elsif @step == '4'
+            @concept = Concept.find(@concept_id)
+            @unit_concepts = UnitConcept.where('concept_id = ?', @concept_id)     
         end    
         
     end
+    
+    def show
+        @unit_concept = UnitConcept.find(params[:id])
+        @unit_concept_descs = @unit_concept.unit_concept_descs.order('desc_type')
+    end    
 
 end
