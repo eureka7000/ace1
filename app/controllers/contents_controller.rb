280c16cc172bcb7621025f0e1c2992f53f424d89
blazingnky <blazingnky@gmail.com>
2016-06-06 11:22:25 +0900

    유닛컨셉 들어갈 때 난이도 화면 추가, 학습창 이미지가 순서대로 나오게 바꿈

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -9,19 +9,25 @@ class ContentsController < ApplicationController
         @category = params[:category]
         @sub_category = params[:sub_category]
         @concept_id = params[:concept_id]
-        
+
+
         if @step == '3'
             @concepts = Concept.where('category = ? and sub_category = ?', @category, @sub_category)
+            @student = 'middle'
+
         elsif @step == '4'
             @concept = Concept.find(@concept_id)
-            @unit_concepts = UnitConcept.where('concept_id = ?', @concept_id)     
+            @unit_concepts = UnitConcept.where('concept_id = ?', @concept_id)
+
+            @student = params[:student]
+            @level = params[:level]
         end    
         
     end
     
     def show
         @unit_concept = UnitConcept.find(params[:id])
-        @unit_concept_descs = @unit_concept.unit_concept_descs.order(:id)
+        @unit_concept_descs = @unit_concept.unit_concept_descs.order(:id).reorder(:file_name)
 
         link_query = "select a.name, a.id, a.code from unit_concepts a, unit_concept_descs b " +
                      "where b.unit_concept_id = #{@unit_concept.id} " +
@@ -30,8 +36,7 @@ class ContentsController < ApplicationController
         # @links = ActiveRecord::Base.connection.execute(link_query)
         @links = UnitConceptDesc.find_by_sql(link_query)
         @videos = @unit_concept.unit_concept_descs.where('desc_type=4').order(:memo)
-        
-        
+
     end    
     
 end
