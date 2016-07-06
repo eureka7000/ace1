b04d305c2af9c330770ae1743868cef28aca7d54
Esteban Jung <mcjung@eurekamath.co.kr>
2016-07-06 15:30:37 +0900

    유사문제

--- app/controllers/unit_concepts_controller.rb
+++ app/controllers/unit_concepts_controller.rb
@@ -83,6 +83,6 @@ class UnitConceptsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def unit_concept_params
-      params.require(:unit_concept).permit(:code, :name, :level, :concept_id, :grade, :exercise_yn)
+      params.require(:unit_concept).permit(:code, :name, :level, :concept_id, :grade, :exercise_yn, :related_unit_concept_id)
     end
 end
