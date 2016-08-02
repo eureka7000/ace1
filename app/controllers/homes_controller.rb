29d6577b8a7ff8777b45acf79fc2fea37b7fa811
Esteban Jung <mcjung@eurekamath.co.kr>
2016-08-02 16:52:43 +0900

    12121212

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,7 +6,7 @@ class HomesController < ApplicationController
 
         unless current_user.nil?
             if current_user.user_types[0].user_type == 'school teacher' || 'mento'
-                @questions_number = Question.where('to_user_id = ? and confirm_yn = ?', current_user.id, 'N').count()
+                @questions_number = Question.where('to_user_id = ? and confirm_yn = ?', current_user.id, 'N').count
 
                 @session_yn = 'Y'
             end
