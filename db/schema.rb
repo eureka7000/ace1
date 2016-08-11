d742c0645dc3eada359b1d68aeaec492a77ca7a8
Esteban Jung <mcjung@eurekamath.co.kr>
2016-08-11 17:56:06 +0900

    222222

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809103940) do
+ActiveRecord::Schema.define(version: 20160810045411) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "name",                  limit: 255
@@ -296,6 +296,7 @@ ActiveRecord::Schema.define(version: 20160809103940) do
     t.integer  "school_id",              limit: 4
     t.string   "user_auth",              limit: 255
     t.datetime "expire_date"
+    t.string   "user_img",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
