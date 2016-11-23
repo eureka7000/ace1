703e9e14b293d3f3bd192f4012a62d786cabe828
NaKwangyup <blazingnky@gmail.com>
2016-11-23 16:52:21 +0900

    Listing Payments 검색 기능 추가

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -13,6 +13,17 @@ class Payment < ActiveRecord::Base
         "GBP" => "&#163;",
         "CHF" => "&#67;",
         "EUR" => "&#8364;"
-	}    
-    
+	}
+
+	SERVICE_NAME = {
+		'1Month' => '1 Month',
+		'2Months' => '2 Months',
+		'3Months' => '3 Months',
+		'4Months' => '4 Months',
+		'5Months' => '5 Months',
+		'6Months' => '6 Months',
+		'month' => 'All Months',
+		'교재구입' => 'Textbook'
+	}
+
 end
