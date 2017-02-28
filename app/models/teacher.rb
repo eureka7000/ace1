b41660cc06f1d29015b9dc98f3b0ead9a2e41b42
NaKwangyup <blazingnky@gmail.com>
2017-02-28 17:51:41 +0900

    관리자 학생목록 맴버여부 추

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -5,7 +5,7 @@ class Teacher < ActiveRecord::Base
 
     def self.get_students(school_id, page, teacher_name, student_name, order)
 
-        query = "select tc.user_name as teacher_name, e.user_id as teacher_id, stu.user_name as student_name, stu.id as student_id, stu.email as student_email, stu.sign_in_count, stu.created_at
+        query = "select tc.user_name as teacher_name, e.user_id as teacher_id, stu.user_name as student_name, stu.id as student_id, stu.email as student_email, tc.user_auth, stu.sign_in_count, stu.created_at
                 from users tc, users stu, user_relations d, teachers e
                 where e.school_id = #{school_id}
                 and e.user_id = d.related_user_id
